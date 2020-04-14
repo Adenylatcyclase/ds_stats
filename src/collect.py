5b562946e675b8c31a6948336c11ad1d5031a1fa
Adenylatcyclase <Streicher1993@googlemail.com>
2020-04-14 20:13:59 +0200

    listings only have 1000 entries which makes me platinum sad

--- src/collect.py
+++ src/collect.py
@@ -1,8 +1,9 @@
 import praw
 from os import getenv
 from database import Post, session, User
-from datetime import datetime, timedelta, date
+from datetime import date
 from time import time
+from sqlalchemy.sql.expression import func
 
 
 reddit = praw.Reddit(client_id=getenv('R_CLIENT_ID'),
@@ -10,36 +11,35 @@ reddit = praw.Reddit(client_id=getenv('R_CLIENT_ID'),
                      user_agent='DarkSouls Data Bot')
 
 
-def fetch_x_days(subreddit, days):
-    target = datetime.today() - timedelta(days=days)
-    flag = True
-    while flag:
-        for s in reddit.subreddit(subreddit).new(limit=100):
-            if datetime.fromtimestamp(int(s.created_utc)) < target:
-                flag = False
-                break
+def fetch_last():
+    return session.query(func.max(Post.r_id)).scalar()
 
-            user = session.query(User).filter(User.name == s.author.name)
-            user = user.first()
 
-            if user is None:
-                user = User(name=s.author.name)
-                session.add(user)
+def fetch_x_days(subreddit, days, last=None):
+    param = {"before": f"t3_{last}"} if last is not None else {}
+    for s in reddit.subreddit(subreddit).new(limit=1000, param=param):
 
-            p = Post(title=s.title,
-                     text=s.selftext,
-                     user=user,
-                     link=s.url,
-                     num_comments=s.num_comments,
-                     score=s.score,
-                     r_id=s.id,
-                     date=date.fromtimestamp(s.created_utc)
-                     .strftime("%Y-%m-%d"),
-                     updated=int(time()*1000))
-        print("cycle")
+        user = session.query(User).filter(User.name == s.author.name).first()
+
+        if user is None:
+            user = User(name=s.author.name)
+            session.add(user)
+
+        p = Post(title=s.title,
+                 text=s.selftext,
+                 user=user,
+                 link=s.url,
+                 num_comments=s.num_comments,
+                 score=s.score,
+                 r_id=s.id,
+                 date=date.fromtimestamp(s.created_utc)
+                 .strftime("%Y-%m-%d"),
+                 updated=int(time()*1000))
+
         session.add(p)
         session.commit()
 
 
 print("running yo")
-fetch_x_days("DarkSouls2", 30)
+print(fetch_last())
+# fetch_x_days("DarkSouls2", 30)
