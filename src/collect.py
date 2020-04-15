ce4382e67c9ffc0b25b157f3b0fe22691ccf481c
Adenylatcyclase <Streicher1993@googlemail.com>
2020-04-15 11:01:42 +0200

    error handling and fixes

--- src/collect.py
+++ src/collect.py
@@ -29,31 +29,23 @@ def fetch_x_days(sub, sub_id, last=None):
     i = 0
 
     for s in reddit.subreddit(sub).new(limit=1000, params=param):
+        author = getattr(s, "author", {})
 
-        # user = session.query(User).filter(User.name == s.author.name).first()
-
-        # if user is None:
-        # user = User(name=s.author.name)
-        # session.add(user)
-
-        if s.author is None:
-            username = ""
-            user_id = -1
-        else:
-            username = s.author.name
-            user_id = s.author.id
-
-        p = Post(title=s.title,
-                 user_id=user_id,
-                 username=username,
-                 link=s.url,
-                 num_comments=s.num_comments,
-                 score=s.score,
-                 r_id=int(s.id, 36),
-                 date=date.fromtimestamp(s.created_utc)
-                 .strftime("%Y-%m-%d"),
-                 updated=int(time()*1000),
-                 sub_id=sub_id)
+        try:
+            p = Post(title=s.title,
+                     user_id=int(getattr(author, "id", "-1"), 36),
+                     username=getattr(author, "name", ""),
+                     link=s.url,
+                     num_comments=s.num_comments,
+                     score=s.score,
+                     r_id=int(s.id, 36),
+                     date=date.fromtimestamp(s.created_utc)
+                     .strftime("%Y-%m-%d"),
+                     updated=int(time()*1000),
+                     sub_id=sub_id)
+        except Exception as e:
+            print(f"oopsie\n{e}")
+            continue
 
         session.add(p)
 
@@ -64,6 +56,7 @@ def fetch_x_days(sub, sub_id, last=None):
 print("running yo")
 
 for sub, sub_id in subreddit_list:
-    last = fetch_last(sub)
+    last = fetch_last(sub_id)
+    print(last)
     fetch_x_days(sub, sub_id, last)
-    print("cycle yo")
+    print(f"cycle yo {last}")
